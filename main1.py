d81f29d2b7523f1b9d28cd2981b3755a4b88fa89
prashant5nov <prashantvelocity123@gmail.com>
2023-03-08 22:12:22 +0530

    first commit

---
+++ main1.py
@@ -0,0 +1,37 @@
+"""
+Hello world application of Flask
+
+Command - `flask --app main run`
+
+- HOME URL
+
+https://swapi.dev
+http://127.0.0.1:5000
+
+
+- RELATIVE
+
+/api/films
+
+- ABSOLUTE URL
+
+
+"""
+
+from flask import Flask
+
+
+# `Flask` is a class we use to instantiate an application
+app = Flask(__name__)
+
+
+# First http GET request
+@app.route("/")
+def hello_world():
+    return "<p>Hello world!</p>"
+
+
+# SECOND http GET request
+@app.route("/greeting")
+def custom_message():
+    return "<h1>Good morning</h1>"
